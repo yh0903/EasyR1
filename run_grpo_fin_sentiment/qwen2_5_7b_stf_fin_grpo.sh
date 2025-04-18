6b225ce0f353e252dbdbbbaf6d2b6012dac2b793
yh0903 <652963574@qq.com>
2025-04-17 22:28:28 -0700

    Update qwen2_5_7b_stf_fin_grpo.sh

--- run_grpo_fin_sentiment/qwen2_5_7b_stf_fin_grpo.sh
+++ run_grpo_fin_sentiment/qwen2_5_7b_stf_fin_grpo.sh
@@ -3,9 +3,9 @@ set -x
 MODEL_PATH=yh0903/financial-reasoning-model-sft  # replace it with your local file path
 
 python3 -m verl.trainer.main \
-    config=run_grpo_fin_sentiment/config.yaml \
-    data.train_files= \
-    data.val_files=hiyouga/math12k@test \
+    config=examples/config.yaml \
+    data.train_files=./data/financial_reasoning_sentiment_dataset_grpo_train.jsonl \
+    data.val_files=./data/financial_reasoning_sentiment_dataset_grpo_val.jsonl \
     worker.actor.model.model_path=${MODEL_PATH} \
-    trainer.experiment_name=qwen2_5_7b_math_grpo \
-    trainer.n_gpus_per_node=8
+    trainer.experiment_name=qwen2_5_7b_sft_finsentiment_grpo \
+    trainer.n_gpus_per_node=4
