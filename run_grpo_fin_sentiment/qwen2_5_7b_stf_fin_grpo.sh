cb6ac6fe1d8d071b0b3d9598444393c9ddef8a28
yh0903 <652963574@qq.com>
2025-04-17 23:21:24 -0700

    Update qwen2_5_7b_stf_fin_grpo.sh

--- run_grpo_fin_sentiment/qwen2_5_7b_stf_fin_grpo.sh
+++ run_grpo_fin_sentiment/qwen2_5_7b_stf_fin_grpo.sh
@@ -5,7 +5,7 @@ MODEL_PATH=yh0903/financial-reasoning-model-sft  # replace it with your local fi
 python3 -m verl.trainer.main \
     config=./run_grpo_fin_sentiment/config.yaml \
     data.train_files=./data/financial_reasoning_sentiment_dataset_grpo_val_mini.parquet \
-    data.val_files=./data/financial_reasoning_sentiment_dataset_grpo_val.parquet \
+    data.val_files=./data/financial_reasoning_sentiment_dataset_grpo_val_mini.parquet \
     worker.actor.model.model_path=${MODEL_PATH} \
     trainer.experiment_name=qwen2_5_7b_sft_finsentiment_grpo \
     trainer.n_gpus_per_node=4
